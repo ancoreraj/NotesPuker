05b09ef74bfafc4b6a370ba50d2a4c35a05a64c3
Rahul Tiwari <rahultwr0005@gmail.com>
2021-03-20 23:21:52 +0530

    Updated search routing

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -72,11 +72,11 @@ router.get('/:year/:branch', ensureAuth , async (req,res)=>{
 
 })
 
-router.post('/:year/:branch', (req, res) => {
+router.post('/:year/:branch', async (req, res) => {
     try {
         const userId = req.body.id
         const title = req.body.title
-        Pdfs.findOne({user: userId, title: title}, (err, foundPdf) => {
+        await Pdfs.findOne({user: userId, title: title}, (err, foundPdf) => {
             if(err) {
                 console.log(err);
             }
@@ -152,7 +152,7 @@ router.post('/uploadfile',ensureAuth,(req,res)=>{
 
 //=================Search routing====================================
 
-router.post("/:year/:branch/search", (req, res) => {
+router.post("/search/:year/:branch", async (req, res) => {
     const searchQuery = req.body.searchQuery;
     const year = req.params.year
     const branch = req.params.branch
@@ -161,27 +161,43 @@ router.post("/:year/:branch/search", (req, res) => {
     var branch_sliced = result.charAt(0).toUpperCase() + result.slice(1);
     // console.log(searchQuery);
 
-    Pdfs.find({title: {$regex: `${searchQuery}`, $options: 'i'}, year: year, college: college}, (err, foundPdf) => {
-        if(err) {
-            console.log(err);
-        }
-        else {
-            // console.log(foundPdf);
-            if(foundPdf.length === 0) {
-                // alert("No Pdfs found for given title.\nTry to enter correct title");
-                res.redirect(`/dashboard/${year}/${branch}`);
+    if(branch === 1) {
+        await Pdfs.find({title: {$regex: `${searchQuery}`, $options: 'i'}, year: year, college: college}, (err, foundPdf) => {
+            if(err) {
+                console.log(err);
             }
             else {
-                res.render("category", {
+                // console.log(foundPdf);
+                res.render("search", {
                     branch_sliced : branch_sliced,
                     college : college,
                     branch: branch,      
                     year : year,
+                    searchQuery: searchQuery,
                     pdfs : foundPdf
                 });
             }
-        }
-    })
+        });
+    }
+    else {
+        await Pdfs.find({title: {$regex: `${searchQuery}`, $options: 'i'}, year: year, branch: branch, college: college}, (err, foundPdf) => {
+            if(err) {
+                console.log(err);
+            }
+            else {
+                // console.log(foundPdf);
+                res.render("search", {
+                    branch_sliced : branch_sliced,
+                    college : college,
+                    branch: branch,      
+                    year : year,
+                    searchQuery: searchQuery,
+                    pdfs : foundPdf
+                });
+            }
+        })
+    }
+    
 })
 
 
