9bbb7987099792cbaed1498ca687d9fac328f9d6
Rahul Tiwari <rahultwr0005@gmail.com>
2021-03-24 21:43:04 +0530

    added url validation

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -113,7 +113,7 @@ router.post('/:year/:branch', async (req, res) => {
 //To upload the file
 router.post('/',ensureAuth,(req,res)=>{
   try{
-      console.log("hello");
+    //   console.log("hello");
     const driveUrl = req.body.driveUrl
     const title = req.body.title
     const year = req.body.year
@@ -138,7 +138,7 @@ router.post('/',ensureAuth,(req,res)=>{
     })
 
     // res.redirect('/dashboard')
-    res.send("hello");
+    res.send({status: "success"});
   } catch (err){
     console.log(err)
 
